a1bbb3e6dc985e069abf8726e6d48c23b0c7a375
Sahithrao153 <sahith.andel@gmail.com>
2016-02-11 19:18:52 -0500

    svar3

--- HW_1_1.py
+++ HW_1_1.py
@@ -1,10 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plot
-
-#myarray is to load data from the given dataset
-from numpy.ma import shape
-
-X,Y = np.loadtxt('svar-set1.dat', unpack=True, usecols=[0,1])
+#X,Y is to load data from the given dataset
+X,Y = np.loadtxt('svar-set3.dat', unpack=True, usecols=[0,1])
 plot.plot(X,Y,"o")
 plot.xlabel("X")
 plot.ylabel("Y")
