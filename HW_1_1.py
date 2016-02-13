fbf9892a3c3a7496a9929f69bf8f7d607811ba8f
sahithrao153 <sahith.andel@gmail.com>
2016-02-13 17:18:10 -0500

    polyfit updated

--- HW_1_1.py
+++ HW_1_1.py
@@ -4,7 +4,8 @@ from sklearn.linear_model import LinearRegression
 #X,Y is to load data from the given dataset
 lm = LinearRegression()
 X,Y = np.loadtxt('svar-set2.dat', unpack=True, usecols=[0,1])
-lm.fit(X[:,np.newaxis],Y)
+"""lm.fit(X[:,np.newaxis],Y)
+
 plt.scatter(X,Y)
 plt.xlabel("X")
 plt.ylabel("Y")
@@ -12,8 +13,12 @@ plt.title("Linear regression")
 plt.plot(X, lm.predict(X[:,np.newaxis]), color='brown')
 plt.show()
 
+"""
+op3 = np.polyfit(X,Y,6)
+print op3
+yf = np.polyval(np.poly1d(op3), Y)
+#print yf
 
-
-
+print max(Y-yf)
 
 
