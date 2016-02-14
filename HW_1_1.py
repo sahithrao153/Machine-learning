c768e8a259c88951d97cf53a004b4013cf70ef23
sahithrao153 <sahith.andel@gmail.com>
2016-02-13 23:17:58 -0500

    Have to split train and test data

--- HW_1_1.py
+++ HW_1_1.py
@@ -3,23 +3,17 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 #X,Y is to load data from the given dataset
 lm = LinearRegression()
-X,Y = np.loadtxt('svar-set1.dat', unpack=True, usecols=[0,1])
-"""lm.fit(X[:,np.newaxis],Y)
+X,Y = np.loadtxt('svar-set2.dat', unpack=True, usecols=[0,1])
 
-plt.scatter(X,Y)
+
+coef = np.polyfit(X,Y,1)
+poly_func = np.poly1d(coef,variable="x")
+ys = poly_func(X)
 plt.xlabel("X")
 plt.ylabel("Y")
-plt.title("Linear regression")
-plt.plot(X, lm.predict(X[:,np.newaxis]), color='brown')
+plt.scatter(X,Y)
+plt.plot(X,ys,'->',color='red')
 plt.show()
 
-"""
-coef = np.polyfit(X,Y,1)
 
-poly_func = np.poly1d(coef,variable="x")
-
-print poly_func
-#plt.scatter(X,Y)
-
-#plt.show()
 
