1636bb640d72d2e856fc88657bb0311266d47e54
sahithrao153 <sahith.andel@gmail.com>
2016-02-14 11:33:29 -0500

    cost function

--- HW_1_1.py
+++ HW_1_1.py
@@ -1,9 +1,7 @@
-import numpy as np
+'''import numpy as np
 import matplotlib.pyplot as plt
-from sklearn import cross_validation, linear_model
 from sklearn.cross_validation import train_test_split
 from sklearn.linear_model import LinearRegression
-#X,Y is to load data from the given dataset
 
 lm = LinearRegression()
 X,Y = np.loadtxt('svar-set2.dat', unpack=True, usecols=[0,1])
@@ -16,4 +14,39 @@ plt.ylabel("Y")
 plt.scatter(X_test,Y_test)
 plt.plot(X_test,ys,color='red')
 plt.show()
+'''
+from numpy import loadtxt, zeros, ones
+from pylab import plot,title, xlabel, ylabel
 
+
+#Evaluate the linear regression
+def cost_function(X, y, theta):
+    m = y.size
+    predictions = X.dot(theta).flatten()
+    square_Errors = (predictions - y) ** 2
+    J = (1.0 / (2 * m)) * square_Errors.sum()
+    return J
+
+
+data = loadtxt('svar-set1.dat')
+
+
+plot(data[:, 0], data[:, 1], marker='o', c='b')
+title('Single variable')
+xlabel('X')
+ylabel('Y')
+
+X = data[:, 0]
+y = data[:, 1]
+
+#training data size
+m = y.size
+
+#Adding ones to X (interception data)
+it = ones(shape=(m, 2))
+it[:, 1] = X
+#Initialize theta parameters
+theta = zeros(shape=(2, 1))
+
+#compute and display initial cost
+print cost_function(it, y, theta)
