f62bc225271ee333193b220c407d35bd81cf4642
sahithrao153 <sahith.andel@gmail.com>
2016-02-15 11:56:31 -0500

    plotting train and test data

--- HW_1_1.py
+++ HW_1_1.py
@@ -2,35 +2,47 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cross_validation import train_test_split
 
-files_list = ['svar-set1.dat','svar-set2.dat','svar-set3.dat','svar-set4.dat']
-print 'files list is :\n',files_list
+files_list = ['svar-set1.dat', 'svar-set2.dat', 'svar-set3.dat', 'svar-set4.dat']
+print 'files list is :\n', files_list
 f_index = int(raw_input('enter the filename index from the list from 1 to 4 '))
 degree_poly = int(raw_input('enter the degree of the polynomial from 1 to n '))
-X,Y = np.loadtxt(files_list[f_index-1], unpack=True, usecols=[0,1])
+X, Y = np.loadtxt(files_list[f_index - 1], unpack=True, usecols=[0, 1])
 
 
-def split_test_data(x,y):
-    X_train, X_test, Y_train, Y_test = train_test_split(x,y,test_size=0.25, random_state=42)
-    coef = np.polyfit(X_test,Y_test,degree_poly)
-    poly_func = np.poly1d(coef,variable="x")
-    ys = poly_func(X_test)
+def split_train_test_data(x, y):
+    X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size=0.25, random_state=42)
+    # to show train data
+    coef_t = np.polyfit(X_test, Y_test, degree_poly)
+    poly_func_t = np.poly1d(coef_t, variable="x")
+    ys = poly_func_t(X_train)
+    plt.title('train data')
     plt.xlabel("X")
     plt.ylabel("Y")
-    plt.scatter(X_test,Y_test)
-    plt.plot(X_test,ys,color='green')
+    plt.scatter(X_train, Y_train)
+    plt.plot(X_train, ys, color='green')
     plt.show()
+    # to show test data
+    coef = np.polyfit(X_test, Y_test, degree_poly)
+    poly_func = np.poly1d(coef, variable="x")
+    ys = poly_func(X_test)
+    plt.title('test data')
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    plt.scatter(X_test, Y_test)
+    plt.plot(X_test, ys, color='green')
+    plt.show()
+
 
 def cross_valid_k():
     print "k"
 
-def find_MSE():
+
+def find_mse():
     print "MSE"
 
-split_test_data(X,Y)
+
+split_train_test_data(X, Y)
 
 cross_valid_k()
 
-find_MSE()
-
-
-
+find_mse()
