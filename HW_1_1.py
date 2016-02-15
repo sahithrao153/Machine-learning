c19f79b3bb1c53a2ae9779c0117044bc61c31b44
sahithrao153 <sahith.andel@gmail.com>
2016-02-15 15:56:50 -0500

    plotting train and test data1

--- HW_1_1.py
+++ HW_1_1.py
@@ -6,12 +6,17 @@ files_list = ['svar-set1.dat', 'svar-set2.dat', 'svar-set3.dat', 'svar-set4.dat'
 print 'files list is :\n', files_list
 f_index = int(raw_input('enter the filename index from the list from 1 to 4 '))
 degree_poly = int(raw_input('enter the degree of the polynomial from 1 to n '))
+test_amount = float(raw_input('enter the test data size from 0 to 1 '))
 X, Y = np.loadtxt(files_list[f_index - 1], unpack=True, usecols=[0, 1])
+plt.title('data')
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.scatter(X, Y)
+plt.show()
 
-
-def split_train_test_data(x, y):
-    X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size=0.25, random_state=42)
-    # to show train data
+def split_train_test_data(x, y ,testsize):
+    X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size=test_amount, random_state=20)
+    # to plot train data
     coef_t = np.polyfit(X_test, Y_test, degree_poly)
     poly_func_t = np.poly1d(coef_t, variable="x")
     ys = poly_func_t(X_train)
@@ -21,7 +26,7 @@ def split_train_test_data(x, y):
     plt.scatter(X_train, Y_train)
     plt.plot(X_train, ys, color='green')
     plt.show()
-    # to show test data
+    # to plot test data
     coef = np.polyfit(X_test, Y_test, degree_poly)
     poly_func = np.poly1d(coef, variable="x")
     ys = poly_func(X_test)
@@ -41,7 +46,7 @@ def find_mse():
     print "MSE"
 
 
-split_train_test_data(X, Y)
+split_train_test_data(X, Y, test_amount)
 
 cross_valid_k()
 
