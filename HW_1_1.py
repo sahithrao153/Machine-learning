f25ee06b05c47ca24f0504ea66057ad834ea6e3f
sahithrao153 <sahith.andel@gmail.com>
2016-02-17 16:35:44 -0500

    No cross Validation

--- HW_1_1.py
+++ HW_1_1.py
@@ -1,53 +1,72 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.cross_validation import train_test_split
 
-files_list = ['svar-set1.dat', 'svar-set2.dat', 'svar-set3.dat', 'svar-set4.dat']
-print 'files list is :\n', files_list
-f_index = int(raw_input('enter the filename index from the list from 1 to 4 '))
-degree_poly = int(raw_input('enter the degree of the polynomial from 1 to n '))
-test_amount = float(raw_input('enter the test data size from 0 to 1 '))
-X, Y = np.loadtxt(files_list[f_index - 1], unpack=True, usecols=[0, 1])
-plt.title('data')
-plt.xlabel("X")
-plt.ylabel("Y")
-plt.scatter(X, Y)
-plt.show()
 
-def split_train_test_data(x, y ,testsize):
-    X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size=test_amount, random_state=20)
-    # to plot train data
-    coef_t = np.polyfit(X_test, Y_test, degree_poly)
-    poly_func_t = np.poly1d(coef_t, variable="x")
-    ys = poly_func_t(X_train)
-    plt.title('train data')
-    plt.xlabel("X")
-    plt.ylabel("Y")
-    plt.scatter(X_train, Y_train)
-    plt.plot(X_train, ys, color='green')
-    plt.show()
-    # to plot test data
-    coef = np.polyfit(X_test, Y_test, degree_poly)
-    poly_func = np.poly1d(coef, variable="x")
-    ys = poly_func(X_test)
-    plt.title('test data')
-    plt.xlabel("X")
-    plt.ylabel("Y")
-    plt.scatter(X_test, Y_test)
-    plt.plot(X_test, ys, color='green')
+def find_mse_train_data(Z, theta, Y, train_size):
+    Z = Z[0:int(train_size * len(Y))]
+    Y = Y[0:int(train_size * len(Y))]
+    ys = np.dot(theta.T, Z.T)
+    diff = (ys.T - Y) ** 2
+    msError = sum(diff) / len(Y)
+    return msError
+
+
+# this function is to find the mean squared error for training data.
+def find_mse_test_data(Z, theta, Y, test_size):
+    Z = Z[int(len(Y) - (200 * test_size)):len(Y)]
+    Y = Y[int(len(Y) - (200 * test_size)):len(Y)]
+    ys = np.dot(theta.T, Z.T)
+    diff = (ys.T - Y) ** 2
+    msError = sum(diff) / len(Y)
+    return msError
+
+
+# this function is to plot the testing data.
+def plot_data(Z, theta, X, Y, test_size):
+    Z_test = Z[int(len(Y) - (200 * test_size)):len(Y)]
+    X_test = X[int(len(Y) - (200 * test_size)):len(Y)]
+    Y_test = np.dot(theta.T, Z_test.T)
+    Y_test = Y_test.reshape(Y_test.size, 1)
+    plt.scatter(X_test, Y[int(len(Y) - (200 * test_size)):len(Y)], color="blue")
+    plt.plot(X_test, Y_test, 'go-', )
     plt.show()
 
 
-def cross_valid_k():
-    print "k"
+# this is to return the theta and Z values using the polynomial function.
+def poly_function(X, Y, Z, degree_poly, test_size):
+    for i in range(degree_poly - 1):
+        X = X * X
+        Z = np.append(Z, X, axis=1)
+    Z_train = Z[0:int(len(Y) - (200 * test_size))]
+    Y = Y[0:int(len(Y) - (200 * test_size))]
+    theta = np.dot(np.dot(np.linalg.inv(np.dot(Z_train.T, Z_train)), Z_train.T), Y)
+
+    return Z, theta
 
 
-def find_mse():
-    print "MSE"
+def main():
+    files_list = ['datasets/svar-set1.dat', 'datasets/svar-set2.dat', 'datasets/svar-set3.dat',
+                  'datasets/svar-set4.dat']
+    print 'files list is :\n', files_list
+    f_index = int(raw_input('enter the filename index from the list from 1 to 4 '))
+    degree_poly = int(raw_input('enter the degree of the polynomial from 1 to n '))
+    test_size = float(raw_input('enter the test size from 0.0 to 1.0 '))
+    train_size = 1.0 - test_size
+    X, Y = np.loadtxt(files_list[f_index - 1], unpack=True, usecols=[0, 1])
+    ''' plt.title('data')
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    plt.scatter(X, Y)
+    plt.show()'''
+    X = X.reshape(X.size, 1)
+    Y = Y.reshape(Y.size, 1)
+    Z_ones = np.ones((X.size, 1), dtype=np.int)
+    Z = np.append(Z_ones, X, axis=1)
+    Z, theta = poly_function(X, Y, Z, degree_poly, test_size)
+    print "MSE for test data", find_mse_test_data(Z, theta, Y, test_size)
+    print "MSE for train data", find_mse_train_data(Z, theta, Y, train_size)
+    plot_data(Z, theta, X, Y, test_size)
 
 
-split_train_test_data(X, Y, test_amount)
-
-cross_valid_k()
-
-find_mse()
+if __name__ == '__main__':
+    main()
